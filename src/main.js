8a1111c7be1324d3e9213fab437437b931ea625e
Jorge <cabezakiwiserrano22@gmail.com>
2017-12-31 14:18:14 +0100

    inicializar sudoku y llamar load

---
+++ src/main.js
@@ -0,0 +1,30 @@
+var sudoku[8][8];
+
+
+function main(){
+	iniSudoku (1);
+}
+
+
+function iniSudoku (n){
+	sudoku [0][0]=8;
+	sudoku [0][2]=9;
+	sudoku [0][4]=7;
+	sudoku [0][5]=5;
+	sudoku [0][8]=1;
+	sudoku [1][2]=6;
+	sudoku [1][6]=5;
+	sudoku [2][1]=3;
+	sudoku [2][3]=1;
+	sudoku [2][7]=9;
+	sudoku [2][8]=9;
+	sudoku [3][0]=6;
+	sudoku [3][0]=8;
+	sudoku [3][0]=8;
+	sudoku [0][0]=8;
+	sudoku [0][0]=8;
+	sudoku [0][0]=8;
+	sudoku [0][0]=8;
+	sudoku [0][0]=8;
+
+}
